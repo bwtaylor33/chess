c6523c2a6eeef7a603f7206e7b0e311a0dcd8f55
Bryce Taylor <bwtaylor33@gmail.com>
2026-02-25 21:30:27 -0700

    completed UserDAO

---
+++ server/src/main/java/dataaccess/InMemoryUserDAO.java
@@ -0,0 +1,46 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.util.HashMap;
+
+public class InMemoryUserDAO implements UserDAO {
+
+    public void createUser(UserData userData) throws DataAccessException {
+        if (users.containsKey(userData.getUsername())) {
+            throw new DataAccessException("Username already in use: " + userData.getUsername());
+        }
+
+        users.put(userData.getUsername(), userData);
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        if (!users.containsKey(username)) {
+            throw new DataAccessException("User not found: " + username);
+        }
+
+        return users.get(username);
+    }
+
+    public void updateUser(UserData userData) throws DataAccessException {
+        if (!users.containsKey(userData.getUsername())) {
+            throw new DataAccessException("User not found: " + userData.getUsername());
+        }
+
+        users.put(userData.getUsername(), userData);
+    }
+
+    public void deleteUser(String username) throws  DataAccessException {
+        if (!users.containsKey(username)) {
+            throw new DataAccessException("User not found: " + username);
+        }
+
+        users.remove(username);
+    }
+
+    public void clearAllUsers() throws DataAccessException {
+        users = new HashMap<>();
+    }
+
+    private HashMap<String, UserData> users = new HashMap<>();
+}
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,7 +1,12 @@
 package dataaccess;
 
+import model.UserData;
+
 public interface UserDAO {
 
-    public void createUser(User user) throws DataAccessException;
-    public User getUser(String username) throws DataAccessException;
+    public void createUser(UserData userData) throws DataAccessException;
+    public UserData getUser(String username) throws DataAccessException;
+    public void updateUser(UserData userData) throws DataAccessException;
+    public void deleteUser(String username) throws  DataAccessException;
+    public void clearAllUsers() throws DataAccessException;
 }
