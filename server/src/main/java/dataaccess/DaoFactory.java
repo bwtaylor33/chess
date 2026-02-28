b4ce4921c9950575b36fe2bd3cf2beae57f06ae9
Bryce Taylor <bwtaylor33@gmail.com>
2026-02-27 23:11:59 -0700

    code quality updates

--- server/src/main/java/dataaccess/DaoFactory.java
+++ server/src/main/java/dataaccess/DaoFactory.java
@@ -1,41 +1,41 @@
 package dataaccess;
 
 /**
- * Supports centralized access to all DAO objects. Constant defined here determines
+ * Supports centralized access to all Dao objects. Constant defined here determines
  * whether a sql database is used or in memory storage.
  */
 public class DaoFactory {
 
-    public static AuthTokenDao getAuthTokenDAO() {
+    public static AuthTokenDao getAuthTokenDao() {
 
         if (USE_IN_MEMORY_DATA_STORE) {
-            return authTokenDAO;
+            return authTokenDao;
         }
 
         throw new RuntimeException("Sql access not implemented");
     }
 
-    public static GameDao getGameDAO() {
+    public static GameDao getGameDao() {
 
         if (USE_IN_MEMORY_DATA_STORE) {
-            return gameDAO;
+            return gameDao;
         }
 
         throw new RuntimeException("Sql access not implemented");
     }
 
-    public static UserDao getUserDAO() {
+    public static UserDao getUserDao() {
 
         if (USE_IN_MEMORY_DATA_STORE) {
-            return userDAO;
+            return userDao;
         }
 
         throw new RuntimeException("Sql access not implemented");
     }
 
-    private static final UserDao userDAO = new InMemoryUserDao();
-    private static final GameDao gameDAO = new InMemoryGameDao();
-    private static final AuthTokenDao authTokenDAO = new InMemoryAuthTokenDao();
+    private static final UserDao userDao = new InMemoryUserDao();
+    private static final GameDao gameDao = new InMemoryGameDao();
+    private static final AuthTokenDao authTokenDao = new InMemoryAuthTokenDao();
 
     private static final boolean USE_IN_MEMORY_DATA_STORE = true;
 }
