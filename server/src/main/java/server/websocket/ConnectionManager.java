fedc301427fcb51290311e1e659cdb0eb7f5a0a0
Bryce Taylor <bwtaylor33@gmail.com>
2026-04-11 19:33:32 -0600

    adding send to ConnectionManager

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,14 +1,13 @@
 package server.websocket;
 
-import org.eclipse.jetty.websocket.api.Session;
-import websocket.messages.ServerMessage;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
+import org.eclipse.jetty.websocket.api.Session;
+
+import websocket.messages.ServerMessage;
 
 public class ConnectionManager {
-    public final ConcurrentHashMap<Integer, ArrayList<Session>> connections = new ConcurrentHashMap<>();
 
     public void add(int gameID, Session session) {
         ArrayList<Session> set = connections.computeIfAbsent(gameID, k -> new ArrayList<>());
@@ -20,17 +19,35 @@ public class ConnectionManager {
         set.remove(session);
     }
 
+    public void send(Session session, ServerMessage serverMessage) throws IOException {
+        String message = serverMessage.toString();
+
+        System.out.println("Sending direct message: " + serverMessage.getServerMessageType());
+        if (serverMessage instanceof websocket.messages.ErrorMessage){
+            websocket.messages.ErrorMessage errMsg = (websocket.messages.ErrorMessage)serverMessage;
+            System.out.println("errorMessage: " + errMsg.getErrorMessage());
+        }
+        synchronized(session){
+            session.getRemote().sendString(message);
+        }
+    }
+
     public void broadcast(int gameID, Session excludeSession, ServerMessage serverMessage) throws IOException {
 
-        String msg = serverMessage.toString();
+        String message = serverMessage.toString();
+        System.out.println("Broadcasting message: " + serverMessage.getServerMessageType());
         ArrayList<Session> set = connections.computeIfAbsent(gameID, k -> new ArrayList<>());
 
         for (Session s : set) {
             if (s.isOpen()) {
                 if (!s.equals(excludeSession)) {
-                    s.getRemote().sendString(msg);
+                    synchronized(s){
+                        s.getRemote().sendString(message);
+                    }
                 }
             }
         }
     }
+
+    private final ConcurrentHashMap<Integer, ArrayList<Session>> connections = new ConcurrentHashMap<>();
 }
